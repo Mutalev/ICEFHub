e08e9e7844aba384a09f2443332007cea831a46b
Lev Stukov <lev090105@gmail.com>
2025-09-28 15:41:41 +0300

    sdfsf

--- Tg.py
+++ Tg.py
@@ -363,13 +363,13 @@ def on_click(message):
         # markup.row(btn1,btn2)
         # bot.send_message(message.chat.id, 'Выберете действие', reply_markup=markup)
     if message.text == '🗓 Получить расписание(pdf)':
-        with open('3-курс-22.09-27.09.pdf', 'rb') as f:
+        with open('3-курс-29.09-04.10.pdf', 'rb') as f:
             bot.send_document(message.chat.id, f)
         # if message.from_user.id == 584787190:
         #     bot.send_message(message.chat.id, 'Купи билет в Ереван Лёвчику')
         bot.send_message(1894542070, f'@{message.from_user.username}')
     if message.text == '🗓 Получть расписание(pdf)':
-        with open('3-курс-22.09-27.09.pdf', 'rb') as f:
+        with open('3-курс-29.09-04.10.pdf', 'rb') as f:
             bot.send_document(message.chat.id, f)
         bot.send_message(1894542070, f'@{message.from_user.username}')
     if message.text == 'ℹ️ Office hours':
